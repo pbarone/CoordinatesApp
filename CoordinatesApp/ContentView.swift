dde76c0616f952b0597631c20ca8a38e792a1ddd
Paolo Barone <pbarone@live.com>
2025-05-13 17:09:03 -0400

    Initial Commit

---
+++ CoordinatesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoordinatesApp
+//
+//  Created by Paolo Barone on 5/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoordinatesApp/CoordinatesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatesAppApp.swift
+//  CoordinatesApp
+//
+//  Created by Paolo Barone on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
